d09ad07ecd7322e9b64d87ac6f835cc1466e6257
Adam Froehlich <adamcfro@gmail.com>
2022-02-11 15:23:34 -0500

    fixed documentation

--- JavaScript_Solutions/warmup1/frontBack.js
+++ JavaScript_Solutions/warmup1/frontBack.js
@@ -1,12 +1,9 @@
-/*
-Given a string, return a new string where the first and last chars have been exchanged.
-*/
-
 /**
+ * Return a new string where the first and last chars have been exchanged.
  * 
- * @param {String} string - a given string
- * @returns - given string with first and last characters swapped
+ * @param {String} str   a string
+ * @returns {String}     a reconstructed string
  */
-function frontBack (string) {
-  return string.slice(-1) + string.slice(1, -1) + string.slice(0, 1);
+function frontBack (str) {
+  return str.slice(-1) + str.slice(1, -1) + str.slice(0, 1);
 }
