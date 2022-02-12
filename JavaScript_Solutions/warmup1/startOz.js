4c57c189a39c99a16441207d698b4678819d1923
Adam Froehlich <adamcfro@gmail.com>
2022-02-12 11:56:54 -0500

    Solution for CodingBat problem, startOz

---
+++ JavaScript_Solutions/warmup1/startOz.js
@@ -0,0 +1,16 @@
+/**
+ * Checks the first two positions of a string to see if they match 'o' for index 1, and 'z' for index 2. Each of the characters that matches are added to a new string and returned.
+ * 
+ * @param {String} str      A string parameter
+ * @returns {String}        Returns a new string
+ */
+function startOz (str) {
+  let newString = '';
+  if (str[0] === 'o') {
+    newString += 'o';
+  } 
+  if (str[1] === 'z') {
+    newString += 'z';
+  }
+  return newString;
+}
