218f44eccb2fab141714f3375b9bd20133d759bc
Adam Froehlich <adamcfro@gmail.com>
2022-02-12 12:38:07 -0500

    Solution for CodingBat problem, endUp

---
+++ JavaScript_Solutions/warmup1/endUp.js
@@ -0,0 +1,13 @@
+/**
+ * Returns a string with the last 3 characters capitalized.
+ * 
+ * @param {String} str      A string parameter
+ * @returns {String}        Returns a new string
+ */
+function endUp (str) {
+  if (str.length < 3) {
+    return str.toUpperCase();
+  } else {
+    return str.substring(0, str.length - 3) + str.substring(str.length - 3).toUpperCase();
+  }
+}
