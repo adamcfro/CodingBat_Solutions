96021042c6604e3e1a512ee7e68e577bdc861996
Adam Froehlich <adamcfro@gmail.com>
2022-02-13 09:56:04 -0500

    Solution for CodingBat problem, last2

---
+++ JavaScript_Solutions/warmup2/last2.js
@@ -0,0 +1,16 @@
+/**
+ * Returns the number of times the last two characters in the string appears elsewhere in the string.
+ * 
+ * @param {String} str      A string parameter
+ * @returns {Number}        Returns a count
+ */
+function last2 (str) {
+  let count = 0;
+  let last2Chars = str.substring(str.length - 2);
+  for (let i = 0; i < str.length - 2; i++) {
+    if (str.substring(i, i + 2) === last2Chars) {
+      count += 1;
+    }
+  }
+  return count;
+}
