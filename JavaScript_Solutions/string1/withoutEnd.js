850623be5c9607929ad2d08e99f45e521d2c7fae
Adam Froehlich <adamcfro@gmail.com>
2022-02-13 15:07:56 -0500

    Solution for CodingBat problem, withoutEnd

---
+++ JavaScript_Solutions/string1/withoutEnd.js
@@ -0,0 +1,9 @@
+/**
+ * Returns the parameter string without the first and last characters.
+ * 
+ * @param {String} str        A string parameter
+ * @returns {String}          Returns a string
+ */
+function withoutEnd (str) {
+  return str.substring(1, str.length - 1);
+}
