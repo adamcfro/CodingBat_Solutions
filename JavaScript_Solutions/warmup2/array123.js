022d6e1e4b86d0fc402c422961d0a528f58a8092
Adam Froehlich <adamcfro@gmail.com>
2022-02-13 10:23:07 -0500

    Solution for CodingBat problem, array123

---
+++ JavaScript_Solutions/warmup2/array123.js
@@ -0,0 +1,14 @@
+/**
+ * Checks if the sequence of numbers [1, 2, 3] occurs in the array parameter.
+ * 
+ * @param {Array} arr       An array parameter
+ * @returns {Boolean}       Returns a Boolean
+ */
+function array123 (arr) {
+  for (let i = 0; i < arr.length - 2; i++) {
+    if (JSON.stringify(arr.slice(i, i + 3)) === JSON.stringify([1, 2, 3])) {
+      return true;
+    } 
+  }
+  return false;
+}
