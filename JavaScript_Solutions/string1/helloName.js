663a23cc592c7e500ed0a6676a966b277b2591e7
Adam Froehlich <adamcfro@gmail.com>
2022-02-13 14:02:38 -0500

    Solution for CodingBat problem, helloName

---
+++ JavaScript_Solutions/string1/helloName.js
@@ -0,0 +1,9 @@
+/**
+ * Returns a string greeting using a given name.
+ * 
+ * @param {String} name     A name (first or last)
+ * @returns {String}        Returns a string greeting
+ */
+function helloName (name) {
+  return `Hello ${name}!`;
+}
