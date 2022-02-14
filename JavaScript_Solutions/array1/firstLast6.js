f346bf4df74d07117f4331b261938d3a00d720db
Adam Froehlich <adamcfro@gmail.com>
2022-02-14 14:58:42 -0500

    Solution for CodingBat problem, firstLast6

---
+++ JavaScript_Solutions/array1/firstLast6.js
@@ -0,0 +1,12 @@
+/**
+ * Returns true if the first or last element in an array of numbers is 6.
+ * 
+ * @param {Array} arr         An array parameter
+ * @returns {Boolean}         Returns true if 6 at front or back of array
+ */
+function firstLast6 (arr) {
+  if (arr[0] === 6 || arr[arr.length - 1] === 6) {
+    return true;
+  }
+  return false;
+}
