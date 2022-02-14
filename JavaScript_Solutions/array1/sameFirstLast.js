f39379f50530826676a4a46a03b1173da26bb6a1
Adam Froehlich <adamcfro@gmail.com>
2022-02-14 15:03:53 -0500

    Solution for CodingBat problem, sameFirstLast

---
+++ JavaScript_Solutions/array1/sameFirstLast.js
@@ -0,0 +1,12 @@
+/**
+ * Returns true if an array has at least one element and the first and last elements are the same.
+ * 
+ * @param {Array} arr         An array parameter
+ * @returns {Boolean}         Returns a Boolean
+ */
+function sameFirstLast (arr) {
+  if (arr.length >= 1 && arr[0] === arr[arr.length - 1]) {
+    return true;
+  }
+  return false;
+}
