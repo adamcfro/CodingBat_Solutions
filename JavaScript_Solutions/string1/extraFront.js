9b21915ad5de1cd13d152cdd195513dc9471b8ba
Adam Froehlich <adamcfro@gmail.com>
2022-02-14 10:43:24 -0500

    Solution for CodingBat problem, extraFront

---
+++ JavaScript_Solutions/string1/extraFront.js
@@ -0,0 +1,13 @@
+/**
+ * Returns a string made up of 3 copies of the first 2 characters of the original string. If there are fewer than 2 characters, returns 3 copies of whatever is there.
+ * 
+ * @param {String} str        A string parameter
+ * @returns {String}          Returns a modified string
+ */
+function extraFront (str) {
+  if (str.length < 2) {
+    return str.repeat(3);
+  } else {
+    return str.substring(0, 2).repeat(3);
+  }
+}
