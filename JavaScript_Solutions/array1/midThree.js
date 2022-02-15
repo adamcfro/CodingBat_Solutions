80c3a3f9cf633d2460efdc2a02f50ce80bde8d81
Adam Froehlich <adamcfro@gmail.com>
2022-02-15 10:35:35 -0500

    Solution for CodingBat problem, midThree

---
+++ JavaScript_Solutions/array1/midThree.js
@@ -0,0 +1,10 @@
+/**
+ * Returns the middle three elements of an odd length array.
+ * 
+ * @param {Array} arr         An array parameter
+ * @returns {Array}           Returns a modified array
+ */
+function midThree (arr) {
+  let middleIndex = Math.floor(arr.length / 2) - 1;
+  return arr.slice(arr[middleIndex - 1], arr[middleIndex + 2]);
+}
