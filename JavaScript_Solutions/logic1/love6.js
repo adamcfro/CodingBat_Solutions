231bcd121b35b107561248d50e3de786cd818057
Adam Froehlich <adamcfro@gmail.com>
2022-02-15 15:11:03 -0500

    Solution for CodingBat problem, love6

---
+++ JavaScript_Solutions/logic1/love6.js
@@ -0,0 +1,10 @@
+/**
+ * Returns true if either parameter is 6, or the sum or difference between the two parameter is 6.
+ * 
+ * @param {Number} num1       First number parameter
+ * @param {Number} num2       Second number parameter
+ * @returns 
+ */
+function love6 (num1, num2) {
+  return ((num1 === 6) || (num2 === 6) || (Math.abs(num1 - num2) === 6) || (num1 + num2 === 6));
+}
