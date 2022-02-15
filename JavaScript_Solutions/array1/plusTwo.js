ec8c75dc4e25ef4a6a486b3d7b1eafe47a205bcb
Adam Froehlich <adamcfro@gmail.com>
2022-02-15 10:25:09 -0500

    Solution for CodingBat problem, plusTwo

---
+++ JavaScript_Solutions/array1/plusTwo.js
@@ -0,0 +1,10 @@
+/**
+ * Returns a single array containing all elements from both parameters.
+ * 
+ * @param {Array} arr1        First array parameter 
+ * @param {Array} arr2        Second array parameter
+ * @returns {Array}           Returns a concatenated array
+ */
+function plusTwo (arr1, arr2) {
+  return arr1.concat(arr2);
+}
