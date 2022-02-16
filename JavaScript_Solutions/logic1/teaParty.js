4d394e4ba7788b58a8dfa9b8a6802aab35494005
Adam Froehlich <adamcfro@gmail.com>
2022-02-16 10:33:37 -0500

    Solution for CodingBat problem, teaParty

---
+++ JavaScript_Solutions/logic1/teaParty.js
@@ -0,0 +1,18 @@
+/**
+ * Returns 0 if either parameter is less than 5, returns 2 if either parameter is double the other parameter, and returns 1 otherwise.
+ * 
+ * @param {Number} tea        The number of tea
+ * @param {Number} candy      The number of candy
+ * @returns {Number}          The party rating
+ */
+function teaParty (tea, candy) {
+  let partyRating;
+  if (tea < 5 || candy < 5) {
+    partyRating = 0;
+  } else if (tea >= (candy * 2) ||(candy >= (tea * 2))) {
+    partyRating = 2;
+  } else {
+    partyRating = 1;
+  }
+  return partyRating;
+}
