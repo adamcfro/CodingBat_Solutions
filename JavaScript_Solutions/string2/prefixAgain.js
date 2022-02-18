026eace03ebeb68554960b8b82737e1aebf81ff4
Adam Froehlich <adamcfro@gmail.com>
2022-02-18 15:17:29 -0500

    Solution for CodingBat problem, prefixAgain

---
+++ JavaScript_Solutions/string2/prefixAgain.js
@@ -0,0 +1,16 @@
+/**
+ * Returns true if the first n characters appear elsewhere in the string.
+ * 
+ * @param {String} str        A string parameter
+ * @param {Number} n          Number of characters in substring
+ * @returns {Boolean}         Returns a Boolean
+ */
+function prefixAgain (str, n) {
+  let prefix = str.substring(0, n);
+  for (let i = 1; i < str.length; i++) {
+    if (str.substring(i, i + n) === prefix) {
+      return true;
+    }
+  }
+  return false;
+}
