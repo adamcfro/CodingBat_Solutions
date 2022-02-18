15ea1535cfcee2ad18ecbbd87598b368457f1e77
Adam Froehlich <adamcfro@gmail.com>
2022-02-18 14:32:28 -0500

    Solution for CodingBat problem, xyBalance

---
+++ JavaScript_Solutions/string2/xyBalance.js
@@ -0,0 +1,17 @@
+/**
+ * Returns true if for every 'x' that appears in the parameter string, there appears a 'y' later in the string.
+ * 
+ * @param {String} str        A string parameter
+ * @returns {Boolean}         Returns a Boolean
+ */
+function xyBalance (str) {
+  let sentinel = true;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === 'x') {
+      sentinel = false;
+    } else if (str[i] === 'y') {
+      sentinel = true;
+    }
+  }
+  return sentinel;
+}
