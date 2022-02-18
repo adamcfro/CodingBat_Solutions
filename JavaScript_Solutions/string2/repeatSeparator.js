2041de1e52cff3e6df473d902797382ff627f9c7
Adam Froehlich <adamcfro@gmail.com>
2022-02-18 15:11:16 -0500

    Solution for CodingBat problem, repeatSeparator

---
+++ JavaScript_Solutions/string2/repeatSeparator.js
@@ -0,0 +1,18 @@
+/**
+ * Returns a string made up of the word parameter separated by the sep parameter n number of times.
+ * 
+ * @param {String} word       A string parameter
+ * @param {String} sep        A separator string
+ * @param {Number} n          Number of iterations
+ * @returns {String}          Returns a new string
+ */
+function repeatSeparator (word, sep, n) {
+  let newStr = '';
+  for (let i = 0; i < count; i++) {
+    newStr += word;
+    if (i + 1 < count) {
+      newStr += sep;
+    }
+  }
+  return newStr;
+}
