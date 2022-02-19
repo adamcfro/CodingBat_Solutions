7f3a151b7299633183250ecbaf04795c651aa67a
Adam Froehlich <adamcfro@gmail.com>
2022-02-19 10:56:47 -0500

    Solution for CodingBat problem, sameEnds

---
+++ JavaScript_Solutions/string3/sameEnds.js
@@ -0,0 +1,15 @@
+/**
+ * Returns the longest substring that appears at both the front and back of the string parameter.
+ * 
+ * @param {String} str        A string parameter
+ * @returns {String}          A substring
+ */
+function sameEnds (str) {
+  let longestSub = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str.substring(0, i) === str.substring(str.length - i)) {
+      longestSub = str.substring(0, i);
+    }
+  }
+  return longestSub;
+}
