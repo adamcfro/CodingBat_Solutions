95312ea44030116ed170e3d92b7062252d29434d
Adam Froehlich <adamcfro@gmail.com>
2022-02-19 10:08:31 -0500

    Solution for CodingBat problem, withoutString

---
+++ JavaScript_Solutions/string3/withoutString.js
@@ -0,0 +1,25 @@
+/**
+ * Returns a new string where any instance of the parameter remove that occurs in the original string have been removed.
+ * 
+ * @param {String} baseStr    A string parameter
+ * @param {String} remove     Substring to remove
+ * @returns {String}          Returns a new string
+ */
+function withoutString (baseStr, remove) {
+  let newStr = '';
+  for (let i = 0; i < baseStr.length; i++) {
+    if (baseStr.substring(i, i + remove.length) === remove) {
+      i += remove.length - 1;
+    } else {
+      newStr += baseStr[i];
+    }
+  }
+  return newStr;
+}
+
+
+
+
+// go through string
+// set length of remove parameter
+// if str.substring() === remove, skip characters
