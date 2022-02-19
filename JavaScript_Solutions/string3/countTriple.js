0c849ec75430074413671b01caca9b779c998ac5
Adam Froehlich <adamcfro@gmail.com>
2022-02-19 10:28:24 -0500

    Solution for CodingBat problem, countTriple

---
+++ JavaScript_Solutions/string3/countTriple.js
@@ -0,0 +1,15 @@
+/**
+ * Returns a count of the number of times a character appears three times in a row.
+ * 
+ * @param {String} str        A string parameter 
+ * @returns {Number}          A count number
+ */
+function countTriple (str) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1] && str[i] === str[i + 2]) {
+      count += 1;
+    }
+  }
+  return count;
+}
