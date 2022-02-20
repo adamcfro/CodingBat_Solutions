ca8f939aee89a457e4178b0cbd79f98863451481
Adam Froehlich <adamcfro@gmail.com>
2022-02-20 10:07:26 -0500

    Solution for CodingBat problem, countEvens

---
+++ JavaScript_Solutions/array2/countEvens.js
@@ -0,0 +1,15 @@
+/**
+ * Returns the amount of times an even number appears in an array.
+ * 
+ * @param {Array} arr         An array of numbers
+ * @returns {Number}          Returns a count
+ */
+function countEvens (arr) {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      total += 1;
+    }
+  }
+  return total;
+}
