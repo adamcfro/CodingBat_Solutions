d41b0964aecad52d961373b7017bff185cea782b
Adam Froehlich <adamcfro@gmail.com>
2022-02-21 10:24:41 -0500

    Solution for CodingBat problem, fizzArray2

---
+++ JavaScript_Solutions/array2/fizzArray2.js
@@ -0,0 +1,13 @@
+/**
+ * Returns a new array made up of the string versions of the numbers 0 through n-1.
+ * 
+ * @param {Number} n          A number to iterate over
+ * @returns {Array}           Returns an array of strings
+ */
+function fizzArray2 (n) {
+  let newArr = [];
+  for (let i = 0; i < n; i++) {
+    newArr.push(i.toString());
+  }
+  return newArr;
+}
