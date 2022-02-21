20678efe8d825b53b5fee5b77091bc9f1de96b73
Adam Froehlich <adamcfro@gmail.com>
2022-02-21 09:39:32 -0500

    Solution for CodingBat problem, sum67

---
+++ JavaScript_Solutions/array2/sum67.js
@@ -0,0 +1,20 @@
+/**
+ * Returns the sum of numbers in an array, ignoring sections that start with 6 and extending to the next instance of 7.
+ * 
+ * @param {Array} arr         An array of numbers
+ * @returns {Number}          Returns a sum
+ */
+function sum67 (arr) {
+  let total = 0;
+  let sentinel = false;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 6 && !sentinel) {
+      total += arr[i];
+    } else if (arr[i] === 6) {
+      sentinel = true;
+    } else if (arr[i] === 7) {
+      sentinel = false;
+    }
+  }
+  return total;
+}
