9c001f349271cdbb96d91bafd80427fab1ab608b
Adam Froehlich <adamcfro@gmail.com>
2022-02-21 09:42:20 -0500

    Solution for CodingBat problem, has22

---
+++ JavaScript_Solutions/array2/has22.js
@@ -0,0 +1,14 @@
+/**
+ * Returns true if an array contains a 2 next to another 2.
+ * 
+ * @param {Array} arr         An array of numbers
+ * @returns {Boolean}         Returns a Boolean
+ */
+function has22 (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 2 && arr[i + 1] === 2) {
+      return true;
+    }
+  }
+  return false;
+}
