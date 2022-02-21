b01b70fa092a8262ac55a1507fdb599a93b18c87
Adam Froehlich <adamcfro@gmail.com>
2022-02-21 11:21:42 -0500

    Solution for CodingBat problem, isEverywhere

---
+++ JavaScript_Solutions/array2/isEverywhere.js
@@ -0,0 +1,15 @@
+/**
+ * Returns true if for every pair of adjacent elements in the array, at least one of the pair is the number parameter.
+ * 
+ * @param {Array} arr         An array of numbers
+ * @param {Number} num        A number parameter
+ * @returns {Boolean}         Returns a Boolean
+ */
+function isEverywhere (arr, num) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== num && arr[i + 1] !== num && arr[i + 1] !== undefined) {
+      return false;
+    }
+  }
+  return true;
+}
