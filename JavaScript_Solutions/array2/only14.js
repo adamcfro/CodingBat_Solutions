c1b18f1adacdd956cf0cd5418e8f2edddea1c8b3
Adam Froehlich <adamcfro@gmail.com>
2022-02-21 10:27:40 -0500

    Solution for CodingBat problem, only14

---
+++ JavaScript_Solutions/array2/only14.js
@@ -0,0 +1,14 @@
+/**
+ * Returns true if every element in an array is either a 1 or a 4.
+ * 
+ * @param {Array} arr         An array of numbers
+ * @returns {Boolean}         Returns a Boolean
+ */
+function only14 (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 1 && arr[i] !== 4) {
+      return false;
+    }
+  }
+  return true;
+}
