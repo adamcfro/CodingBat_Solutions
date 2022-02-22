bc2bb92b824de8c76d9756e28bd8923d920fb951
Adam Froehlich <adamcfro@gmail.com>
2022-02-22 10:44:33 -0500

    Solution for CodingBat problem, twoTwo

---
+++ JavaScript_Solutions/array2/twoTwo.js
@@ -0,0 +1,14 @@
+/**
+ * Returns true if every 2 that appears in the array is next to another 2.
+ * 
+ * @param {Array} arr         An array of numbers
+ * @returns {Boolean}         Returns a Boolean
+ */
+function twoTwo (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 2 && (arr[i - 1] !== 2 && arr[i + 1] !== 2)) {
+      return false;
+    }
+  }
+  return true;
+}
