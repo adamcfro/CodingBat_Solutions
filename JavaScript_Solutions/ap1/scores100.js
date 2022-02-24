44ce8069f35eeee551e7a89c1aacfb778a68e991
Adam Froehlich <adamcfro@gmail.com>
2022-02-24 09:38:29 -0500

    Solution for CodingBat problem, scores100

---
+++ JavaScript_Solutions/ap1/scores100.js
@@ -0,0 +1,14 @@
+/**
+ * Returns true if there are two elements of 100 next to each other in the array.
+ * 
+ * @param {Array} arr         An array of numbers
+ * @returns {Boolean}         Returns a Boolean
+ */
+function scores100 (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 100 && arr[i + 1] === 100) {
+      return true;
+    }
+  }
+  return false;
+}
