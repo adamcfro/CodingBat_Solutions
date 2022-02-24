6c33f32b1ffc9a9cec37bda387f73a28b3b73d0f
Adam Froehlich <adamcfro@gmail.com>
2022-02-24 16:48:05 -0500

    Cleaned up deleted files

--- javascript-solutions-1/string1/right2.js
+++
@@ -1,9 +0,0 @@
-/**
- * Returns a new string with the last two characters moved to the front of the string.
- * 
- * @param {String} str        A string parameter
- * @returns {String}          Returns a modified string
- */
-function right2 (str) {
-  return str.substring(str.length - 2) + str.substring(0, str.length - 2);
-}
