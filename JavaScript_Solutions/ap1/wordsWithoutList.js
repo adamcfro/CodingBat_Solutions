35561412f41dbd5bdeb9179d7e4222b061a44ccf
Adam Froehlich <adamcfro@gmail.com>
2022-02-24 10:31:44 -0500

    Solution for CodingBat problem, wordsWithoutList

---
+++ JavaScript_Solutions/ap1/wordsWithoutList.js
@@ -0,0 +1,16 @@
+/**
+ * Returns a new array where all strings of length n are omitted.
+ * 
+ * @param {Array} arr         An array of strings
+ * @param {Number} n          Target string length
+ * @returns {Array}           Returns a new array of strings
+ */
+function wordsWithoutList (arr, n) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length !== n) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
