3224cd1432ceac47f5b9db96edb6ec4e6fde396e
Adam Froehlich <adamcfro@gmail.com>
2022-02-25 10:50:55 -0500

    Solution for CodingBat problem, sumHeights2

---
+++ javascript-solutions/ap-1/sum-heights-2.js
@@ -0,0 +1,20 @@
+/**
+ * Given start and end indexes of an array, returns the sum of the differences 
+ * between adjacent numbers. However, increases in height count for double.
+ * 
+ * @param {Array} arr         An array of numbers
+ * @param {Number} start      A starting index
+ * @param {Number} end        An ending index
+ * @returns {Number}          Returns a sum
+ */
+function sumHeights2 (arr, start, end) {
+  let altitudeChange = 0;
+  for (let i = start; i < end; i++) {
+    if (arr[i] - arr[i + 1] > 0) {
+      altitudeChange += Math.abs(arr[i] - arr[i + 1]);
+    } else {
+      altitudeChange += Math.abs(arr[i] - arr[i + 1]) * 2;
+    }
+  }
+  return altitudeChange;
+}
