eae2e647896990995af93f29f34e2defeb2f6659
Adam Froehlich <adamcfro@gmail.com>
2022-02-25 10:26:28 -0500

    Solution for CodingBat problem, scoresSpecial

---
+++ javascript-solutions/ap-1/scores-special.js
@@ -0,0 +1,26 @@
+/**
+ * Returns the sum of the largest multiple of 10 from each array parameter.
+ * 
+ * @param {Array} arr1        First array of numbers
+ * @param {Array} arr2        Second array of numbers
+ * @returns {Number}          Returns a sum
+ */
+function scoresSpecial (arr1, arr2) {
+  return largestSpecial(arr1) + largestSpecial(arr2);
+}
+
+/**
+ * Returns the largest multiple of 10 in an array.
+ * 
+ * @param {Array} arr         An array of numbers
+ * @returns {Number}          Returns the largest multiple of 10
+ */
+function largestSpecial (arr) {
+  let largest = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 10 === 0 && arr[i] > largest) {
+      largest = arr[i];
+    }
+  }
+  return largest;
+}
