3e2ce8593f191879771401d26c8665803bf31358
Adam Froehlich <adamcfro@gmail.com>
2022-02-25 10:34:34 -0500

    Solution for CodingBat problem, sumHeights

---
+++ javascript-solutions/ap-1/sum-heights.js
@@ -0,0 +1,15 @@
+/**
+ * Returns the sum of the difference between adjacent numbers, starting at the start index and ending at the end index.
+ * 
+ * @param {Array} arr         An array of numbers
+ * @param {Number} start      A starting index
+ * @param {Number} end        An ending index
+ * @returns {Number}          Returns a sum
+ */
+function sumHeights (arr, start, end) {
+  let totalChange = 0;
+  for (let i = start; i < end; i++) {
+    totalChange += Math.abs(arr[i] - arr[i + 1]);
+  }
+  return totalChange;
+}
