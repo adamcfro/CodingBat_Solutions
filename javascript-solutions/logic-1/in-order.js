7ffa944a2184f1aeef1a0ef00b8e6bf121288afd
Adam Froehlich <adamcfro@gmail.com>
2022-02-26 14:44:07 -0500

    Renamed variable for easier readability, added extra documentation

--- javascript-solutions/logic-1/in-order.js
+++ javascript-solutions/logic-1/in-order.js
@@ -1,14 +1,16 @@
 /**
- * Returns true if num2 is greater than num1, and num3 is greater than num2, unless bOk is true, in which case num2 doesn't need to be greater than num1.
+ * Returns true if num2 is greater than num1, and num3 is greater than num2, 
+ * unless only3 is true, in which case num2 doesn't need to be greater than 
+ * num1.
  * 
- * @param {Number} num1 
- * @param {Number} num2 
- * @param {Number} num3 
- * @param {Boolean} bOk 
+ * @param {Number} num1       First number parameter
+ * @param {Number} num2       Second number parameter
+ * @param {Number} num3       Third number parameter
+ * @param {Boolean} only3     Only num3 needs to be greater than num1      
  * @returns 
  */
-function inOrder (num1, num2, num3, bOk) {
-  if ((bOk && num3 > num2) || (num2 > num1 && num3 > num2)) {
+function inOrder (num1, num2, num3, only3) {
+  if ((only3 && num3 > num2) || (num2 > num1 && num3 > num2)) {
     return true;
   }
   return false;
