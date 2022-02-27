fe69cf7fd331c4b4477542658bb3a20fba1059d7
Adam Froehlich <adamcfro@gmail.com>
2022-02-27 10:52:53 -0500

    Added to documentation

--- javascript-solutions/warmup-1/back-around.js
+++ javascript-solutions/warmup-1/back-around.js
@@ -1,8 +1,9 @@
 /**
- * Returns a new string with the last character of the original string added to the front and back
+ * Returns a new string with the last character of the original string 
+ * added to the front and back.
  * 
- * @param {String} str    A string parameter
- * @returns {String}      Returns a new string
+ * @param {String} str - A string parameter
+ * @returns {String} - Returns a new string
  */
 function backAround (str) {
   return str[str.length - 1] + str + str[str.length - 1];
