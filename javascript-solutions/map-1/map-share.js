bb67954a38fec40feb47b7b4c40efb1220afe030
Adam Froehlich <adamcfro@gmail.com>
2022-04-01 10:29:24 -0400

    Added documentation to parameters of map-share problem

--- javascript-solutions/map-1/map-share.js
+++ javascript-solutions/map-1/map-share.js
@@ -3,8 +3,8 @@
  * if the key 'a' has a value, set the key 'b' to have that same value.
  * In all cases remove the key 'c', leaving the rest of the map unchanged.
  * 
- * @param {Object} obj 
- * @returns {Object}
+ * @param {Object} obj - An object parameter
+ * @returns {Object} - Returns a modified object
  */
 function mapShare (obj) {
   if (obj['a']) {
