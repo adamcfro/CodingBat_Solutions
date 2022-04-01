dd3c45a60d115c8dda608122b9198d540835b7a6
Adam Froehlich <adamcfro@gmail.com>
2022-04-01 10:28:27 -0400

    Added documentation to parameters of map-bully problem

--- javascript-solutions/map-1/map-bully.js
+++ javascript-solutions/map-1/map-bully.js
@@ -2,8 +2,8 @@
  * Modifies and returns the given map as follows: if the key 'a' has a value,
  * set the key 'b' to have that value and set the key 'a' to have the value ''.
  * 
- * @param {Object} obj 
- * @returns {Object}
+ * @param {Object} obj - An object parameter
+ * @returns {Object} - Returns a modified object
  */
  function mapBully (obj) {
   if (obj['a'] !== null) {
