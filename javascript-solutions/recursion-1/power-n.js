0e7d059cfef015168ffc4aec1e5b1d8528a1d0d6
Adam Froehlich <adamcfro@gmail.com>
2022-04-13 11:34:49 -0400

    Added parameter documentation for the powerN problem

--- javascript-solutions/recursion-1/power-n.js
+++ javascript-solutions/recursion-1/power-n.js
@@ -1,9 +1,9 @@
 /**
  * Recursively returns the value of 'base' to the power 'n'.
  * 
- * @param {Number} base 
- * @param {Number} power 
- * @returns 
+ * @param {Number} base - A base number
+ * @param {Number} power - The power to which the base number is raised
+ * @returns {Number} - Returns a squared value
  */
 function powerN (base, power) {
   if (power === 0) {
