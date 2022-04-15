056725f96dfe12e800bd2238cdbb2330173457f6
Adam Froehlich <adamcfro@gmail.com>
2022-04-15 15:05:33 -0400

    Solution for CodingBat problem, stringClean

---
+++ javascript-solutions/recursion-1/string-clean.js
@@ -0,0 +1,19 @@
+/**
+ * Recursively computes and returns a 'cleaned' string, where adjacent
+ * characters that are the same have been reduced to a single character.
+ * 
+ * @param {String} str - A string parameter
+ * @returns {String} - Returns a modified string
+ */
+function stringClean (str) {
+  if (str.length === 0) {
+    return str;
+  }
+  if (str[0] === str[1]) {
+    holdString = str[0];
+    return stringClean(str.substring(1));
+  } else {
+    holdString = '';
+    return str[0] + stringClean(str.substring(1));
+  }
+}
