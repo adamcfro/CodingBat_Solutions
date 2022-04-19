0f2841b11aaaf6fe05c966dcad7601a22043e51f
Adam Froehlich <adamcfro@gmail.com>
2022-04-19 10:06:59 -0400

    Solution for CodingBat problem, groupSum6

---
+++ javascript-solutions/recursion-2/group-sum-6.js
@@ -0,0 +1,19 @@
+/**
+ * Uses recursion to check if it is possible for the numbers in the given
+ * array to sum to the given target.
+ * 
+ * @param {Number} start - The starting index
+ * @param {Number} arr - An array of numbers
+ * @param {Number} target - The target sum
+ * @returns {Boolean} - Returns true if the target can be met
+ */
+function groupSum6 (start, arr, target) {
+  if (start >= arr.length) {
+    return target === 0;
+  }
+  if (arr[start] === 6) {
+    return groupSum6(start + 1, arr, target - 6);
+  }
+  return groupSum6(start + 1, arr, target - arr[start]) ||
+    groupSum6(start + 1, arr, target);
+}
