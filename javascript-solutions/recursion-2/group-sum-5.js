b22dd1b2b6e4322a1876e5064d2315fc2ad7cf36
Adam Froehlich <adamcfro@gmail.com>
2022-04-20 09:15:30 -0400

    Solution for CodingBat problem, groupSum5

---
+++ javascript-solutions/recursion-2/group-sum-5.js
@@ -0,0 +1,25 @@
+/**
+ * Uses recursion to check if the number in the given array can sum up to
+ * the target parameter, with the following restraints:
+ * all multiples of 5 in the array must be included. If the value immediately
+ * following a multiple of 5 is 1, it must not be chosen.
+ * 
+ * @param {Number} start - The starting index
+ * @param {Number} arr - An array of numbers
+ * @param {Number} target - The target sum
+ * @returns {Boolean} - Returns true if the target can be met
+ */
+function groupSum5 (start, arr, target) {
+  if (start >= arr.length) {
+    return target === 0;
+  }
+  if (arr[start] % 5 === 0) {
+    if (start < arr.length && arr[start + 1] === 1) {
+      return groupSum5(start + 2, arr, target - arr[start]);
+    } else {
+      return groupSum5(start + 1, arr, target - arr[start]);
+    }
+  }
+  return groupSum5(start + 1, arr, target - arr[start]) ||
+    groupSum5(start + 1, arr, target);
+}
